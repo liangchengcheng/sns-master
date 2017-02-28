7916da2bd4f96d78c2fb6738240c8485eed8a4af
liangchengcheng <1038127753@qq.com>
2017-02-28 09:49:33 +0800

    add js file
    add js file

---
+++ src/main/webapp/js/guide.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('#ok').live('click',function () {
+        $(this).addClass('loading');
+        $.ajax({
+            url: basePath + '/guide/ok',
+            type: 'GET',
+            dataType: 'json'
+        }).success(function(data){
+            self.location = basePath;
+        });
+    });
+});
